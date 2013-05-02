4e501fe8ba312728fad0f194edacca943f599d0d
Matt Cano <cano.matt@gmail.com>
2013-05-02 16:18:13 -0700

    random letter function and prmitive board creator

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ $('button').on('click', function(){
 });
 
 
-function smallBoard(){
-  var
+
+function randomLetter(){
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var rand = _.random(0, alphabet.length-1);
+  return alphabet[rand];
+};
+
+function board(size){
+  var uniqueLetters = Math.floor(size/2);
+  size = uniqueLetters * 2;
+  var lettersUsed = []
+
+  _(uniqueLetters).times( function(){
+    lettersUsed.push(randomLetter());
+  });
+
+  return lettersUsed;
+
 }
